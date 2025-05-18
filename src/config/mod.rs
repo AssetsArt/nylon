f2586ed52a4b9ff51686e0340490ffb2e3723069
detoro <meanstack20@gmail.com>
2025-05-18 19:44:14 +0700

    Cleanup

--- src/config/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod backend;
-pub mod certs;
-pub mod proxy;
-pub mod runtime;
-pub mod store;
--- src/proxy/constant.rs
+++
@@ -1,2 +0,0 @@
-// static
-pub static WELL_KNOWN_PAHT_PREFIX: &str = "/.well-known/acme-challenge/";
